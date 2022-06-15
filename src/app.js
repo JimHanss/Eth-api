4f5d95fea96b4e5993a6d01edbfd9c9b4191bee0
JIM__HUANG <jimhuang@ideaculture.com>
2022-06-15 14:25:08 +0800

    222

---
+++ src/app.js
@@ -0,0 +1,71 @@
+const Koa = require("koa");
+const app = new Koa();
+const views = require("koa-views");
+const json = require("koa-json");
+const onerror = require("koa-onerror");
+const bodyparser = require("koa-bodyparser");
+const logger = require("koa-logger");
+const session = require("koa-generic-session");
+const cors = require("koa2-cors");
+
+const index = require("./routes/index");
+const users = require("./routes/users");
+
+// error handler
+onerror(app);
+
+// middlewares
+app.use(
+  bodyparser({
+    enableTypes: ["json", "form", "text"],
+  })
+);
+app.use(json());
+app.use(logger());
+app.use(require("koa-static")(__dirname + "/public"));
+
+//session配置
+app.keys = ["liyunfuAAA"]; //密钥用于加密
+app.use(
+  session({
+    //配置cookie
+    cookie: {
+      path: "/",
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
+
+//cors配置
+app.use(
+  cors({
+    origin: "http://localhost:8089", //前端origin
+    credentials: true, //允许跨域带cookie
+  })
+);
+
+app.use(
+  views(__dirname + "/views", {
+    extension: "pug",
+  })
+);
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+// routes
+app.use(index.routes(), index.allowedMethods());
+app.use(users.routes(), users.allowedMethods());
+
+// error-handling
+app.on("error", (err, ctx) => {
+  console.error("server error", err, ctx);
+});
+
+module.exports = app;
