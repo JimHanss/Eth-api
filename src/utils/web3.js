6f6a14631c3f74d6f3dd2e916a48aa9028d28c7f
JIM__HUANG <jimhuang@ideaculture.com>
2022-06-21 14:13:49 +0800

    update

--- src/utils/web3.js
+++ src/utils/web3.js
@@ -1,10 +1,13 @@
 import { createAlchemyWeb3 } from "@alch/alchemy-web3";
 import { ethers } from "ethers";
 
-const privateKey = process.env.PRIVATE_KEY || "";
-const publicKey = process.env.PUBLIC_KEY || "";
-const url = process.env.rinkebyNet;
-const nftUrl = process.env.rinkebyNFTNet;
+const privateKey =
+  "0x26bba027d7a9efb6778edce028539382651fb081353295e0011be24280bcd376";
+const publicKey = "0x5136042e2a35B9c649faD7229cbc2594E8FD856E";
+const url =
+  "https://eth-rinkeby.alchemyapi.io/v2/qx5q8YdLAvVqoOfkm365wMLhQFYOwsWV";
+const nftUrl =
+  "https://eth-rinkeby.alchemyapi.io/nft/v2/qx5q8YdLAvVqoOfkm365wMLhQFYOwsWV";
 
 const alchemyWeb3 = createAlchemyWeb3(url);
 const alchemyNftWeb3 = createAlchemyWeb3(nftUrl);
