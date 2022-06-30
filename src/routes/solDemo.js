6fda5eb891cec2a12a35ea793b02623ddd12d829
JIM__HUANG <jimhuang@ideaculture.com>
2022-06-30 18:38:52 +0800

    add solDemo getNft api

--- src/routes/solDemo.js
+++ src/routes/solDemo.js
@@ -1,5 +1,9 @@
+const { PublicKey } = require("@solana/web3.js");
+import _ from "lodash";
 const { default: getSolNft } = require("../utils/func/solDemo/getSolNft");
 const { default: transfer } = require("../utils/func/solDemo/transfer");
+import { connection } from "../utils/web3";
+import { programs } from "@metaplex/js";
 
 const router = require("koa-router")();
 
@@ -8,6 +12,27 @@ router.prefix("/solDemo");
 router.get("/", function (ctx, next) {
   console.log(ctx.request.query);
   ctx.body = ctx.request.query.param;
+});
+
+router.post("/getNft", async (ctx) => {
+  try {
+    const nfts = await getSolNft(
+      "GcGWaaKhKuNrfjy4xiyqbHTFw46NpCv8VkyXwgBmShbL"
+    );
+
+    const aa = _.map(nfts, (item) => item.toString());
+
+    const tokenMetadata = await programs.metadata.Metadata.findByMint(
+      connection,
+      nfts[0]
+    );
+
+    console.log(tokenMetadata);
+
+    ctx.body = aa;
+  } catch (error) {
+    console.log("getNft", error);
+  }
 });
 
 router.post("/transfer", async (ctx, next) => {
